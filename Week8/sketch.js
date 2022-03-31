76cc28467f1fbd3b0c444bde95dd529e8a40c70f
Kelly Kwong <kelly@Kellys-MBP.fios-router.home>
2022-03-31 19:16:15 -0400

    week8

---
+++ Week8/sketch.js
@@ -0,0 +1,36 @@
+
+
+let img;
+
+function preload() {
+  img = loadImage('https://kellykwg.github.io/Week8/duck.jpeg');
+
+  // Click the > menu to the left and look in
+  // the images directory for more images to try!
+  // Or upload your own image!x
+  // URLs also work, like this:
+  // img = loadImage('https://upload.wikimedia.org/wikipedia/commons/6/69/June_odd-eyed-cat_cropped.jpg');
+}
+function setup() {
+  createCanvas(600, 600);
+
+  // Resize the image so it fits on the screen.
+  img.resize(width, height);
+  
+  background(32);
+  noStroke();
+}
+
+function draw() {
+
+  // Get the color of a random pixel.
+  img.loadPixels();
+  
+  let pixelX = random(width);
+  let pixelY = random(height);
+  let pixelColor = img.get(pixelX, pixelY);
+
+  // Paint that pixel with a circle.
+  fill(pixelColor);
+  circle(pixelX, pixelY, 10);
+}
